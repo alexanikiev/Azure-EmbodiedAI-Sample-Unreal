93a16f6e63c3991078c1977f795fcc40d8541e26
Alex Anikiiev <alexani@microsoft.com>
2023-04-04 15:55:31 -0700

    Added template Cpp project

---
+++ apps/Sample/Source/Sample/Public/SpeechBlueprintAsyncActionBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "SpeechBlueprintAsyncActionBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAMPLE_API USpeechBlueprintAsyncActionBase : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+	
+};
