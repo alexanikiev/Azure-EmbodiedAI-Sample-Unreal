6e3e578453c7964462a87733b94c42d0568a4621
Alex Anikiiev <alexani@microsoft.com>
2023-04-04 16:10:29 -0700

    Added WebApi class template

--- apps/Sample/Source/Sample/Public/SpeechBlueprintAsyncActionBase.h
+++ apps/Sample/Source/Sample/Public/SpeechBlueprintAsyncActionBase.h
@@ -7,11 +7,11 @@
 #include "SpeechBlueprintAsyncActionBase.generated.h"
 
 /**
- * 
+ * Functionality to integrate with Azure Speech SDK (Listen, Speak, etc.)
  */
 UCLASS()
 class SAMPLE_API USpeechBlueprintAsyncActionBase : public UBlueprintAsyncActionBase
 {
 	GENERATED_BODY()
-	
+
 };
---
+++ apps/Sample/Source/Sample/Public/WebApiBlueprintAsyncActionBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "WebApiBlueprintAsyncActionBase.generated.h"
+
+/**
+ * Functionality to integrate with custom or first-party Web APIs (Azure OpenAI ChatGPT, CLU/LUIS, Azure Cognitive Search, etc.)
+ */
+UCLASS()
+class SAMPLE_API UWebApiBlueprintAsyncActionBase : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+};
